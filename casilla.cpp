8d7e73769e5612b3ac96cbf4dbccc9cd425e437f
Lucianogmz <gomezlucianogab@gmail.com>
2025-07-11 11:57:33 -0300

    Tp parte 2

--- casilla.cpp
+++ casilla.cpp
@@ -1,3 +1,14 @@
 #include "casilla.h"
+#include "jugador.h"
 
-casilla::casilla() {}
+casilla::casilla(int numero) : numero(numero) {}
+
+casilla::~casilla(){}
+
+void casilla::accion(jugador& j){
+
+}
+
+int casilla::getNumero() const{
+    int numero;
+}
